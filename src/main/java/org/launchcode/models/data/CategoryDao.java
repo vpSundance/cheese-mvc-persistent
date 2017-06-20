e860d74cca5cd0701049d9ef194f495e2aec2819
Kaley Schenk <kaley.schenk@gmail.com>
2017-06-19 19:16:37 -0500

    Setup Category DAO

---
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -0,0 +1,15 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by Splodey on 6/19/2017.
+ */
+@Repository
+@Transactional
+public interface CategoryDao extends CrudRepository<Category, Integer> {
+}
