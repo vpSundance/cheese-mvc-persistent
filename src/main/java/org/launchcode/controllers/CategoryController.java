253a04bab4a198d66a018085d9341da47195085a
Kaley Schenk <kaley.schenk@gmail.com>
2017-06-21 11:16:12 -0500

    Functionality for adding persistent categories

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,10 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by Splodey on 6/19/2017.
@@ -22,5 +28,23 @@ public class CategoryController {
         model.addAttribute("title", "Categories");
 
         return "category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute(new Category());
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+        if (errors.hasErrors()) {
+            return "category/add";
+        }
+
+        categoryDao.save(category);
+        return "redirect:";
     }
 }
