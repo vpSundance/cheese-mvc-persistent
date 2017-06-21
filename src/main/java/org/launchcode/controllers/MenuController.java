a8fe0bfe379f510b4e110aaaf46c1f72cb43e7ec
Kaley Schenk <kaley.schenk@gmail.com>
2017-06-21 13:53:30 -0500

    Created menu controller class

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,32 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Splodey on 6/21/2017.
+ */
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("title", "Menus");
+        model.addAttribute(menuDao.findAll());
+
+        return "menu/index";
+
+    }
+}
