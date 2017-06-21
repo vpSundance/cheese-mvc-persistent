642e7de6c6cddf601b278cace782af77a6316d84
Kaley Schenk <kaley.schenk@gmail.com>
2017-06-21 13:27:19 -0500

    Added MenuDao  interface

---
+++ src/main/java/org/launchcode/models/data/MenuDao.java
@@ -0,0 +1,15 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.Menu;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by Splodey on 6/21/2017.
+ */
+@Repository
+@Transactional
+public interface MenuDao extends CrudRepository<Menu, Integer> {
+}
