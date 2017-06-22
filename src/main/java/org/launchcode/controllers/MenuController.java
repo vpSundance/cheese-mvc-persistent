ac32e25cdbc6acc0958d717a4da5d88f2f2b2eae
Kaley Schenk <kaley.schenk@gmail.com>
2017-06-22 14:06:37 -0500

    Menu view and add

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by Splodey on 6/21/2017.
@@ -24,9 +30,41 @@ public class MenuController {
     public String index(Model model) {
 
         model.addAttribute("title", "Menus");
-        model.addAttribute(menuDao.findAll());
+        model.addAttribute("menus", menuDao.findAll());
 
         return "menu/index";
 
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @Valid Menu menu, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+
+        return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value = "view/{menuId}", method = RequestMethod.GET)
+    public String viewMenu(Model model, @PathVariable int menuId) {
+
+        Menu menu = menuDao.findOne(menuId);
+
+        model.addAttribute("title", menu.getName());
+        model.addAttribute(menu);
+
+        return "menu/view";
     }
 }
